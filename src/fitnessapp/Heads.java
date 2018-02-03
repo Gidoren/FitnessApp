df392590c3a736c614a0c692d8c1b592ce17fceb
Cameron Saifizadeh <saifizadehcameron@yahoo.com>
2018-02-03 09:43:39 -0800

    fixed heads node lass

--- src/fitnessapp/Heads.java
+++ src/fitnessapp/Heads.java
@@ -1,13 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Owner
- */
-public class Heads {
+  public int frequency;
+    String name;
+    public Heads()
+    {
+        frequency = 0;
+        name = ""; 
+    }
+    public Heads(String S)
+    {
+        frequency = 0;
+        name = S;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public int getFrequency()
+    {
+        return frequency;
+    }
+    public void setName(String S)
+    {
+        name = S;
+    }
+    public void setFrequency(int f)
+    {
+        frequency = f;
+    }
     
 }
