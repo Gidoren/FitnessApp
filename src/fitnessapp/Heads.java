d4167418e63102fa8d3b7aa8212c70dc9a7d3553
MatthewReingold <34205172+MatthewReingold@users.noreply.github.com>
2018-02-03 10:28:51 -0800

    Update Heads.java

--- src/fitnessapp/Heads.java
+++ src/fitnessapp/Heads.java
@@ -1,34 +1,36 @@
-package fitnessapp;
-
-public class Heads
-{
-    public int frequency;
-    String name;
-    public Heads()
-    {
-        frequency = 0;
-        name = ""; 
-    }
-    public Heads(String S)
-    {
-        frequency = 0;
-        name = S;
-    }
-    public String getName()
-    {
-        return name;
-    }
-    public int getFrequency()
-    {
-        return frequency;
-    }
-    public void setName(String S)
-    {
-        name = S;
-    }
-    public void setFrequency(int f)
-    {
-        frequency = f;
-    }
-    
-}
+public class Heads {
+	  public int Work;
+	    String name;
+	    public Heads()
+	    {
+	        Work = 0;
+	        name = ""; 
+	    }
+	    public Heads(String S)
+	    {
+	        Work = 0;
+	        name = S;
+	    }
+	    public String getName()
+	    {
+	        return name;
+	    }
+	    public int getWork()
+	    {
+	        return Work;
+	    }
+	    public void setName(String S)
+	    {
+	        name = S;
+	    }
+	    public void setWork(int w)
+	    {
+	        Work = w;
+	    }
+	    
+	    public void WorkOut(int worked)
+	    {
+	    	Work+=worked;
+	    }
+	    
+	}
