70ea67660d910d0a7ad31d454bb9fac0154df65f
MatthewReingold <34205172+MatthewReingold@users.noreply.github.com>
2018-02-03 12:07:53 -0800

    Update Body.java

--- src/fitnessapp/Body.java
+++ src/fitnessapp/Body.java
@@ -3,28 +3,34 @@ private Muscle[] arms = new Muscle[10];
 private int armsEls = 0;
 private Muscle[] legs = new Muscle[10];
 private int legsEls = 0;
-private Muscle[] chestAndCore = new Muscle[10];
+private Muscle[] chest = new Muscle[10];
 private int chestEls = 0;
 private Muscle[] back = new Muscle[10];
 private int backEls = 0;
-
+private Muscle[] core = new Muscle[10];
+private int coreEls = 0;
 
 public void addMuscle(Muscle x, String name)
 {
-	if(x.getName() == "arms")
+	if(x.getName() == "bicep" || x.getName() == "tricep" || x.getName() == "forearm" || x.getName() == "shoulders")
 	{
 		arms[armsEls] = x;
 		armsEls++;
 	}
-	else if(x.getName() == "legs")
+	else if(x.getName() == "quadriceps" || x.getName() == "gluteus" || x.getName() == "calves" || x.getName() == "hamstrings")
 	{
 		legs[legsEls] = x;
 		legsEls++;
 	}
-	else if(x.getName() == "chestAndCore")
+	else if(x.getName() == "pectoralis major" || x.getName() == "pectoralis minor")
 	{
-		chestAndCore[chestEls] = x;
+		chest[chestEls] = x;
 		chestEls++;
+	}
+	else if(x.getName() == "rectus abdominus" || x.getName() == "obliques" || x.getName() == "serratus")
+	{
+		core[coreEls] = x;
+		armsEls++;
 	}
 	else
 	{
@@ -44,9 +50,9 @@ public Muscle[] getLegs()
 	return legs;
 }
 
-public Muscle[] getChestAndCore()
+public Muscle[] getchest()
 {
-	return chestAndCore;
+	return chest;
 }
 
 public Muscle[] getBack()
