08d1f12ae1c38f64ce5474a70bf1822ddf43134f
Arma15 <kflorio98@gmail.com>
2018-02-03 22:06:51 -0800

    In third activity

---
+++ PersonalTrainer/app/src/main/java/com/example/justi/personaltrainer/Shoulders.java
@@ -0,0 +1,14 @@
+package com.example.justi.personaltrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Shoulders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoulders);
+    }
+}
