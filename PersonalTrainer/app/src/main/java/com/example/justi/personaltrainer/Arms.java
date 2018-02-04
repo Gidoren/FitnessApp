a82f7b3b5a0e3c18b297edc4ad25ea0a8610a9d2
Arma15 <kflorio98@gmail.com>
2018-02-03 21:18:12 -0800

    Arms linked to next activity

---
+++ PersonalTrainer/app/src/main/java/com/example/justi/personaltrainer/Arms.java
@@ -0,0 +1,17 @@
+package com.example.justi.personaltrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Arms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arms);
+        //Button leftArm = (Button)findViewById(R.id.leftArm);
+
+    }
+}
--- PersonalTrainer/app/src/main/java/com/example/justi/personaltrainer/MainActivity.java
+++ PersonalTrainer/app/src/main/java/com/example/justi/personaltrainer/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.justi.personaltrainer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton rightArm;
+    private ImageButton leftArm;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rightArm = (ImageButton)findViewById(R.id.rightArm);
+        rightArm.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                callArms();
+            }
+        });
+
+        leftArm = (ImageButton)findViewById(R.id.leftArm);
+        leftArm.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                callArms();
+            }
+        });
     }
+
+    public void callArms()
+    {
+        Intent startNewActivity = new Intent(this, Arms.class);
+        startActivity(startNewActivity);
+    }
+
 }
